a35f314c5260fe3687dc557b46cfe04d1d65a4c2
tanyazinovieva <zinovievatanya123@gmail.com>
2022-11-30 12:53:32 +0300

    комичу

--- projectTanya/nii/models.py
+++ projectTanya/nii/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Employees(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    inn = models.TextField(null=True, blank=True)
+    position = models.CharField(max_length=50)
+    data = models.DateField(auto_now_add=True, db_index=True)
 # Create your models here.
--- projectTanya/nii/views.py
+++ projectTanya/nii/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from .models import Employees
 
 def index(request):
-    return HttpResponse("Привет! Это твое первое приложение!")
+    s = 'Список объявлений\r\n\r\n\r\n\r\n\r\n\r\n'
+    for employees in Employees.objects.order_by('-data'):
+        s += employees.first_name + '\r\n' + employees.last_name + '\r\n' + employees.middle_name + '\r\n' + employees.inn + '\r\n' + employees.position + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+#def index(request):
+   #return HttpResponse("Привет! Это твое первое приложение!")
+   #добавила \r\n\r\n\r\n, ('-data'), вместо bb employees
