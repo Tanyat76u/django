31e0491c933a2016d80236ecc2f0c1ac70f482cb
tanyazinovieva <zinovievatanya123@gmail.com>
2022-12-05 00:06:59 +0300

    Две модели, админка

--- projectTanya/nii/views.py
+++ projectTanya/nii/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
 from .models import Employees, Projects
-#from .models import Projects
 from django.shortcuts import render
 def index(request):
     employeess = Employees.objects.all()
     projectss = Projects.objects.all()
-        #s += employees.first_name + '\r\n' + employees.last_name + '\r\n' + employees.middle_name + '\r\n' + employees.inn + '\r\n' + employees.position + '\r\n\r\n'
     return render(request, "nii/index.html", {'employeess': employeess, 'projectss': projectss})
