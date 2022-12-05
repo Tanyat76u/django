8c8e397cdc2733bc50c2b3a8779ebee9d2d3c8cb
tanyazinovieva <zinovievatanya123@gmail.com>
2022-12-05 23:24:56 +0300

    сделала админку

--- projectTanya/nii/admin.py
+++ projectTanya/nii/admin.py
@@ -3,9 +3,14 @@ from .models import Employees
 from .models import Projects
 
 class EmployeesAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'middle_name', 'inn', 'position', 'data')
+    list_display = ('first_name', 'last_name', 'middle_name', 'num_ser', 'inn', 'position', 'data')
     list_display_links = ('first_name',)
     search_fields = ('first_name', 'last_name', 'middle_name', 'data')
-    
+
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount_of_financing')
+    list_display_links = ('name',)
+    search_fields = ('name', 'start_date')
+
 admin.site.register(Employees, EmployeesAdmin)
-#admin.site.register(Projects, ProjectsAdmin)
+admin.site.register(Projects, ProjectsAdmin)
