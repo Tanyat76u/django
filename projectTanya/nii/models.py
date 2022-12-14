a269b38089582f3a626ee21a5e41bec7c6ae731d
tanyazinovieva <zinovievatanya123@gmail.com>
2022-12-14 22:21:59 +0300

    все получилось! поправила index_html

--- projectTanya/nii/models.py
+++ projectTanya/nii/models.py
@@ -9,7 +9,8 @@ class Employees(models.Model):
     position = models.CharField(max_length=50, verbose_name='Должность')
     data = models.CharField(max_length=30, verbose_name='Дата рождения')
     def __str__(self):
-        return self.data
+        #return self.first_name
+        return f'{self.first_name} {self.last_name} {self.middle_name}'
         
     class Meta:
         verbose_name_plural = 'Сотрудники'
@@ -21,7 +22,7 @@ class Projects(models.Model):
     start_date = models.CharField(max_length=50, verbose_name='Дата начала')
     end_date = models.CharField(max_length=50, verbose_name='Дата окончания')
     amount_of_financing = models.CharField(max_length=20, verbose_name='Объем финансирования')
-    employeess = models.ManyToManyField(Employees,verbose_name='Сотрудники')
+    employeess = models.ManyToManyField(Employees, verbose_name='Сотрудники')
     def __str__(self):
         return self.name
         
